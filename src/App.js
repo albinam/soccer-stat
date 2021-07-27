95cb63e958ee96d0f83b8d497a7bb9ae0b3602ce
albinosikm <albinosikm@gmail.com>
2021-07-27 21:41:58 +0400

    gh-pages

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     return (
         <div className={styles}>
             <HashRouter>
-                <BrowserRouter>
+                <BrowserRouter basename={process.env.PUBLIC_URL}>
                     <Switch>
                         <Route path='/' exact><Redirect to='/leagues'/>
                         </Route>
