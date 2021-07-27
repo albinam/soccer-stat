a22361bba97fb0dbd5e85fd257a4d81919708d16
albinosikm <albinosikm@gmail.com>
2021-07-27 21:14:31 +0400

    gh-pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./App.css";
-import {BrowserRouter, HashRouter, Route} from "react-router-dom";
+import {BrowserRouter, HashRouter, Route, Switch} from "react-router-dom";
 import Leagues from "./Components/Leagues/Leagues";
 import Teams from "./Components/Teams/Teams";
 import TeamMatches from "./Components/TeamMatches/TeamMatches";
@@ -12,15 +12,19 @@ import TeamsPage from "./Components/TeamsPage/TeamsPage";
 function App() {
     return (
         <div className={styles}>
-            <HashRouter basename={process.env.PUBLIC_URL}>
-                <Route path='/' exact><Redirect to='/leagues'/>
-                </Route>
-                <Route path='/leagues' exact component={Leagues}/>
-                <Route path='/leagues/:leagueId/teams/:team?' exact component={Teams}/>
-                <Route path='/teams/:params?' exact component={TeamsPage}/>
-                <Route path='/teams/:teamId/matches' exact component={TeamMatches}/>
-                <Route path='/leagues/:leagueId/matches' exact component={LeagueMatches}/>
-                <Route path='/leagues/:leagueId/standings' exact component={Standings}/>
+            <HashRouter>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path='/' exact><Redirect to='/leagues'/>
+                        </Route>
+                        <Route path='/leagues' exact component={Leagues}/>
+                        <Route path='/leagues/:leagueId/teams/:team?' exact component={Teams}/>
+                        <Route path='/teams/:params?' exact component={TeamsPage}/>
+                        <Route path='/teams/:teamId/matches' exact component={TeamMatches}/>
+                        <Route path='/leagues/:leagueId/matches' exact component={LeagueMatches}/>
+                        <Route path='/leagues/:leagueId/standings' exact component={Standings}/>
+                    </Switch>
+                </BrowserRouter>
             </HashRouter>
         </div>
     );
