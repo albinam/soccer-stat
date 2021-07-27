a99324e3630a678d1cef3e6fb61288372f77de36
albinosikm <albinosikm@gmail.com>
2021-07-27 16:43:30 +0400

    gh-pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from "react";
 import styles from "./App.css";
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, HashRouter, Route} from "react-router-dom";
 import Leagues from "./Components/Leagues/Leagues";
 import Teams from "./Components/Teams/Teams";
 import TeamMatches from "./Components/TeamMatches/TeamMatches";
-import Home from "./Components/Home/Home";
 import LeagueMatches from "./Components/LeagueMatches/LeagueMatches";
 import Standings from "./Components/Standings/Standings";
-import {Redirect} from "react-router";
+import {Redirect} from "react-router-dom";
 import TeamsPage from "./Components/TeamsPage/TeamsPage";
 
 function App() {
     return (
         <div className={styles}>
-            <BrowserRouter>
+            <HashRouter basename={process.env.PUBLIC_URL}>
                 <Route path='/' exact><Redirect to='/leagues'/>
                 </Route>
                 <Route path='/leagues' exact component={Leagues}/>
@@ -22,7 +21,7 @@ function App() {
                 <Route path='/teams/:teamId/matches' exact component={TeamMatches}/>
                 <Route path='/leagues/:leagueId/matches' exact component={LeagueMatches}/>
                 <Route path='/leagues/:leagueId/standings' exact component={Standings}/>
-            </BrowserRouter>
+            </HashRouter>
         </div>
     );
 }
